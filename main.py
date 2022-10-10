caf450e295315b3ca28310a362ec937d74620b8c
karasik0401 <85022375+karasik0401@users.noreply.github.com>
2022-10-11 00:53:36 +0300

    Изменение интерфейса калькулятора для новых функций

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 from matrix import input_matrix
 from simple import simple_calculator
+from integer import integr, kvad
 
 
 while True:
-    print("выберите что вы хотите сделать:\n1.Матричный калькулятор\n2.Обычный калькулятор\nq чтобы выйти")
+    print("выберите что вы хотите сделать:\n1.Матричный калькулятор\n2.Обычный калькулятор\n3.Интеграл\n4.Квадратное кравнение\nq чтобы выйти")
     choice = input()
     if choice == "1":
         input_matrix()
-    if choice == "2":
+    elif choice == "2":
         simple_calculator()
+    elif choice == "3":
+        print(integr())
+    elif choice == "4":
+        print(kvad())
     if choice == "q":
         break
     else:
