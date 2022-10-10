f8e2cba4ead3d5241bd1b6147f68c27e4f819949
karasik0401 <85022375+karasik0401@users.noreply.github.com>
2022-10-11 00:50:47 +0300

    Создание тестов интеграла и квадратного уравнения

---
+++ Integer_test.py
@@ -0,0 +1,18 @@
+import unittest
+from numbers import Integral
+
+import pytest
+from sympy import integrate
+from sympy.abc import x
+
+from main import integr, kvad
+
+
+def test_integr():
+    expr = x**2
+    assert integrate(expr, x) == x**3/3
+
+def test_kvad():
+    assert kvad(3, -2, 2) == 'Нет корней'
+
+
